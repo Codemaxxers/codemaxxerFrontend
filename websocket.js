ca92f95c169dc02b9a24a4cb3a269e708c68a643
rachit-j <rjaiswal.sd.77@gmail.com>
2024-05-27 23:24:57 -0700

    websockets?

---
+++ websocket.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const socket = new SockJS('http://codemaxxers.stu.nighthawkcodingsociety.com/myhandler');
+    const stompClient = Stomp.over(socket);
+
+    stompClient.connect({}, function (frame) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/messages', function (messageOutput) {
+            showMessageOutput(messageOutput.body);
+        });
+    });
+
+    function sendMessage() {
+        const messageInput = document.getElementById('messageInput').value;
+        stompClient.send("/app/hello", {}, messageInput);
+    }
+
+    function showMessageOutput(messageOutput) {
+        const response = document.getElementById('messages');
+        const p = document.createElement('p');
+        p.style.wordWrap = 'break-word';
+        p.appendChild(document.createTextNode(messageOutput));
+        response.appendChild(p);
+    }
+
+    window.sendMessage = sendMessage; // Make sendMessage available globally
+});
