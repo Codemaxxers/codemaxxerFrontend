760efcf200d4a35bc464fe594351a3c613b49969
Jyustin <supercatsd@gmail.com>
2024-05-29 14:11:25 -0700

    test

--- uri.js
+++ uri.js
@@ -1,8 +1,9 @@
 var uri;
-if (location.hostname === "localhost") {
-    uri = "http://localhost:8032";
-} else if (location.hostname === "127.0.0.1") {
-    uri = "http://localhost:8032";
-} else {
-    uri = "https://codemaxxers.stu.nighthawkcodingsociety.com";
-}
+// if (location.hostname === "localhost") {
+//     uri = "http://localhost:8032";
+// } else if (location.hostname === "127.0.0.1") {
+//     uri = "http://localhost:8032";
+// } else {
+//     uri = "https://codemaxxers.stu.nighthawkcodingsociety.com";
+// }
+uri = "http://localhost:8032";
