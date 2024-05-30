71e86773aa65556d04ffaceb1d934433d595ed5d
Jyustin <supercatsd@gmail.com>
2024-05-29 22:36:20 -0700

    followup

--- assets/js/chatbot.js
+++ assets/js/chatbot.js
@@ -13,7 +13,7 @@ const elements = {
 };
 
 // Define URLs for various API endpoints
-//uri = "http://localhost:8032";
+//uri = "http://localhost:8032"; :)
 uri = "https://codemaxxers.stu.nighthawkcodingsociety.com/";
 const urls = {
   chat: uri+"/aichatbot/chat?message=", // Endpoint for sending a chat message
